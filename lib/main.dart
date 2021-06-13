a0ffc86339beb9c52476256d3cfe58ba9ee1e7db
pratik329 <pratikraut329@gmail.com>
2021-06-13 19:05:49 +0530

    simple_card_with_grid_view

--- lib/main.dart
+++ lib/main.dart
@@ -50,31 +50,19 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: GridView.builder(
-          padding: EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(20.0), //padding with screen Border
           itemCount: 15,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 3,
               crossAxisSpacing: 50.0,
               mainAxisSpacing: 100.0),
-          itemBuilder: (context, index) => Text(
-            "${index + 1}",
-            style: Theme.of(context).textTheme.display1,
+          itemBuilder: (context, i) => Card(
+            color: Colors.blue,
+            child: Center(
+              child: Text("${i + 1}"),
+            ),
           ),
-        )
-        // GridView.extent(
-        //   maxCrossAxisExtent: 50.0,
-        //   crossAxisSpacing: 50.0,
-        //   mainAxisSpacing: 100.0,
-        //   padding: EdgeInsets.all(20.0),
-        //   children: [1, 2, 3, 4, 5, 6, 7, 8, 9.10, 11, 12, 13, 14, 15]
-        //       .map(
-        //         (e) => Text(
-        //           "$e",
-        //           style: Theme.of(context).textTheme.display1,
-        //         ),
-        //       )
-        //       .toList(),
-        // ) // This trailing comma makes auto-formatting nicer for build methods.
+        ) // This trailing comma makes auto-formatting nicer for build methods.
         );
   }
 }
